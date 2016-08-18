fa52eeca5cf0974bd35bf31a433252cba88f11ea
593525559@qq.com <593525559@qq.com>
2016-08-18 16:42:27 +0800

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var http = require('http'); 
 var routes = require('./routes/index');
-
+var partials = require('express-partials');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,9 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //入口
 app.use('/', routes);
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
