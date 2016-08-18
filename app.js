3f3a093089c0d1b305166866037b394b8934b08c
593525559@qq.com <593525559@qq.com>
2016-08-18 16:03:54 +0800

    update

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var http = require('http'); 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -22,8 +23,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//入口
 app.use('/', routes);
-app.use('/users', users);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. 定义网站主页的路由*/
+ router.get('/', function(req, res, next) {
+   res.render('index', { title: '首页' });
+ });
 
-//引入文件夹的别名
+//主页
+ router.get('/home', function(req, res, next) {
+   res.render('home', { title: '主页' });
+ });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '首页' });
-});
+//帮助
+ router.get('/help', function(req, res, next) {
+   res.render('help', { title: '帮助' });
+ });
 
-router.get('app/home', function(req, res, next) {
-  res.render('app/home', { title: '主页' });
-});
+//娱乐
+router.get('/recreation', function(req, res, next) {
+   res.render('recreation', { title: '娱乐' });
+ });
+
+//个人中心
+router.get('/uCenter', function(req, res, next) {
+   res.render('uCenter', { title: '个人中心' });
+ });
+
+//搜索
+router.get('/search', function(req, res, next) {
+   res.render('search', { title: '搜索' });
+ });
+
+//学习
+router.get('/study', function(req, res, next) {
+   res.render('study', { title: '学习' });
+ });
+
+//设置
+router.get('/setting', function(req, res, next) {
+   res.render('setting', { title: '设置' });
+ });
+
+//好友
+router.get('/gFrinds', function(req, res, next) {
+   res.render('gFrinds', { title: '好友' });
+ });
+
+
 module.exports = router;
+
+
